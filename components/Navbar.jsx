6e304af2e0f441d1f361ff7a2677c9f06809f956
AnnlynBright <68070083+AnnlynBright@users.noreply.github.com>
2023-06-10 07:13:01 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,8 +7,8 @@ import { FiPieChart } from 'react-icons/fi';
 
 const Navbar = () => (
     <Flex p='2' borderBottom='1px' borderColor='gray.100'>
-      <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
-        <Link href='/' paddingLeft='2'>Realtor</Link>
+      <Box fontSize='3xl' color='red.400' fontWeight='bold'>
+        <Link href='/' paddingLeft='2'>Annlyn Real Estate</Link>
       </Box>
       <Spacer />
       <Box>
